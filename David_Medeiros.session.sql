cc25fc85d5c18f18d28cfd49979e5e5c5011d4bb
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2025-02-02 21:28:54 -0300

    Adicionando Cabeçalho

--- David_Medeiros.session.sql
+++ David_Medeiros.session.sql
@@ -1,3 +1,11 @@
+/* =======================================================
+   Projeto: Roteiro 01 - Disciplina de Banco de Dados
+   Autor: David Medeiros Santos
+   Data de Criação: 02/02/2025
+   Descrição: Exp´lorando os primeiros comandos SQL DDL
+   Versão: 1.0
+   ======================================================= */
+
 CREATE DATABASE IF NOT EXISTS bd_roteiro_01;
 
 CREATE TABLE IF NOT EXISTS candidato_funcionario(
