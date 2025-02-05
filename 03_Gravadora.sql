14d8fd92a8448b3f8ae396cf56d8efc92de653aa
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2025-02-05 15:38:29 -0300

    Criando banco de dados GRAVADORA

---
+++ 03_Gravadora.sql
@@ -0,0 +1,12 @@
+/* =======================================================
+   Projeto: Roteiro 01 - Disciplina de Banco de Dados
+   Autor: David Medeiros Santos
+   Data de Criação: 02/02/2025
+   Descrição: Explorando os primeiros comandos SQL DDL
+   Versão: 1.0
+   ======================================================= */
+
+
+-- Crianco novo banco de dados caso não exista
+
+CREATE DATABASE IF NOT EXISTS Gravadora;
