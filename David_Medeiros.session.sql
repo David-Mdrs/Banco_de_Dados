837df0bf52b004f438bcb2f52d353f88fcaf6b6d
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2025-02-05 14:37:02 -0300

    Chaves Primárias

--- David_Medeiros.session.sql
+++ David_Medeiros.session.sql
@@ -82,15 +82,16 @@ SHOW TABLES;
 
 ALTER TABLE funcionario ADD CONSTRAINT PK_func PRIMARY KEY (id_func);
 ALTER TABLE candidato ADD CONSTRAINT PK_candidato PRIMARY KEY (id_candidato);
+ALTER TABLE inscricao ADD CONSTRAINT PK_inscricao PRIMARY KEY (id_inscricao);
+ALTER TABLE cargo ADD CONSTRAINT PK_cargo PRIMARY KEY (id_cargo);
+ALTER TABLE teste ADD CONSTRAINT PK_teste PRIMARY KEY (id_teste);
+ALTER TABLE processo_seletivo ADD CONSTRAINT PRIMARY KEY (id_processo);
+ALTER TABLE departamento ADD CONSTRAINT PRIMARY KEY (id_depto);
 
 ALTER TABLE candidato_funcionario ADD CONSTRAINT PK_cand_func PRIMARY KEY(funcionario, candidato);
 ALTER TABLE candidato_funcionario ADD CONSTRAINT FK_cand_func FOREIGN KEY (funcionario) REFERENCES funcionario(id_func);
 ALTER TABLE candidato_funcionario ADD CONSTRAINT FK_cand_func_cand FOREIGN KEY (candidato) REFERENCES candidato(id_candidato);
 
-ALTER TABLE inscricao ADD CONSTRAINT PK_inscricao PRIMARY KEY (id_inscricao);
-ALTER TABLE cargo ADD CONSTRAINT PK_cargo PRIMARY KEY (id_cargo);
-
 ALTER TABLE inscricao ADD CONSTRAINT FK_insc_candidato FOREIGN KEY (candidato) REFERENCES CANDIDATO(id_candidato);
-
 ALTER TABLE inscricao ADD CONSTRAINT FK_insc_cargo FOREIGN KEY (cago) REFERENCES CARGO(id_cargo)
 
