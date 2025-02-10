1cb4eb0215d4d2d97b585553ecc9b9fb5a43090f
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2025-02-10 14:52:25 -0300

    Criando tabelas

--- 02_Vendas.sql
+++ 02_Vendas.sql
@@ -10,3 +10,43 @@
 -- Crianco novo banco de dados caso não exista
 
 CREATE DATABASE IF NOT EXISTS vendas;
+
+
+-- Criando tabelas do banco de dados caso não existam
+
+CREATE TABLE IF NOT EXISTS cliente(
+   codigoCli INT PRIMARY KEY,
+   nome VARCHAR(30) NOT NULL,
+   uf VARCHAR(2) NOT NULL
+);
+DESC cliente;
+
+CREATE TABLE IF NOT EXISTS venda(
+   numero INT PRIMARY KEY,
+   data DATE,
+   total REAL,
+   codigoCli INT NOT NULL,
+   FOREIGN KEY (codigoCli) REFERENCES cliente(codigoCli) 
+);
+DESC venda;
+
+CREATE TABLE IF NOT EXISTS produto(
+   codigo INT PRIMARY KEY,
+   descricao VARCHAR(50) NOT NULL,
+   precoCompra REAL,
+   lucro REAL,
+   quantidade REAL,
+   categoria VARCHAR(30) NOT NULL
+);
+DESC produto;
+
+CREATE TABLE IF NOT EXISTS item_venda(
+   qnt REAL,
+   precoUnitario REAL,
+   codigo INT NOT NULL,
+   numero INT NOT NULL,
+   PRIMARY KEY (codigo, numero),
+   FOREIGN KEY (codigo) REFERENCES produto(codigo),
+   FOREIGN KEY (numero) REFERENCES venda(numero)
+);
+DESC item_venda;
