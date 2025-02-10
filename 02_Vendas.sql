75d151a3d2a888dad8c9615ef6de02f48b5c4cf7
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2025-02-10 14:26:07 -0300

    Criando novo banco de dados

---
+++ 02_Vendas.sql
@@ -0,0 +1,12 @@
+/* =======================================================
+   Projeto: Roteiro 02 - Disciplina de Banco de Dados
+   Autor: David Medeiros Santos
+   Data de Criação: 08/02/2025
+   Descrição: Comandos SQL DDL para sistema simples de vendas
+   Versão: 1.0
+   ======================================================= */
+
+
+-- Crianco novo banco de dados caso não exista
+
+CREATE DATABASE IF NOT EXISTS vendas;
