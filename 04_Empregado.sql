da4194b7cce6276c3dd21c3290f33a4b4e908c36
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2025-02-15 14:22:05 -0300

    Criando novo banco de dados

---
+++ 04_Empregado.sql
@@ -0,0 +1,12 @@
+/* =======================================================
+   Projeto: Empregado - Disciplina de Banco de Dados
+   Autor: David Medeiros Santos
+   Data de Criação: 15/02/2025
+   Descrição: Banco de dados para registro de empregados e suas sessões
+   Versão: 1.0
+   ======================================================= */
+
+
+-- Crianco novo banco de dados caso não exista
+
+CREATE DATABASE IF NOT EXISTS empregado;
