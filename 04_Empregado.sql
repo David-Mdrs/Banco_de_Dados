60fa651f3791752055a7cdf1e4bfc80d1e27b295
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2025-02-15 14:26:01 -0300

    Criando tabelas

--- 04_Empregado.sql
+++ 04_Empregado.sql
@@ -10,3 +10,28 @@
 -- Crianco novo banco de dados caso não exista
 
 CREATE DATABASE IF NOT EXISTS empregado;
+
+
+-- Criando tabelas do banco de dados caso não existam
+
+CREATE TABLE empregado (
+ idemp INTEGER PRIMARY KEY, 
+ nome VARCHAR(20) NOT NULL, 
+ secao INTEGER NOT NULL, 
+ cargo VARCHAR(15) NOT NULL, 
+ chefe INTEGER, 
+ salario DECIMAL(7,2) NOT NULL,
+ comissao DECIMAL(7,2) NOT NULL
+);
+
+CREATE TABLE secao (
+ idsec INTEGER PRIMARY KEY, 
+ nome VARCHAR(20) NOT NULL, 
+ cidade VARCHAR(20) NOT NULL
+);
+
+
+-- Definindo chaves estrangeiras
+
+ALTER TABLE empregado ADD CONSTRAINT FOREIGN KEY (secao) REFERENCES secao(idsec);
+ALTER TABLE empregado ADD CONSTRAINT FOREIGN KEY (chefe) REFERENCES empregado (idemp);
