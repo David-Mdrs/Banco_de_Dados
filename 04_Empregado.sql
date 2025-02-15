ed598bd9b0163a458d099c9cc08dccdba2f2c0c8
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2025-02-15 14:27:21 -0300

    Inserindo dados

--- 04_Empregado.sql
+++ 04_Empregado.sql
@@ -15,19 +15,19 @@ CREATE DATABASE IF NOT EXISTS empregado;
 -- Criando tabelas do banco de dados caso não existam
 
 CREATE TABLE empregado (
- idemp INTEGER PRIMARY KEY, 
- nome VARCHAR(20) NOT NULL, 
- secao INTEGER NOT NULL, 
- cargo VARCHAR(15) NOT NULL, 
- chefe INTEGER, 
- salario DECIMAL(7,2) NOT NULL,
- comissao DECIMAL(7,2) NOT NULL
+   idemp INTEGER PRIMARY KEY, 
+   nome VARCHAR(20) NOT NULL, 
+   secao INTEGER NOT NULL, 
+   cargo VARCHAR(15) NOT NULL, 
+   chefe INTEGER, 
+   salario DECIMAL(7,2) NOT NULL,
+   comissao DECIMAL(7,2) NOT NULL
 );
 
 CREATE TABLE secao (
- idsec INTEGER PRIMARY KEY, 
- nome VARCHAR(20) NOT NULL, 
- cidade VARCHAR(20) NOT NULL
+   idsec INTEGER PRIMARY KEY, 
+   nome VARCHAR(20) NOT NULL, 
+   cidade VARCHAR(20) NOT NULL
 );
 
 
@@ -35,3 +35,23 @@ CREATE TABLE secao (
 
 ALTER TABLE empregado ADD CONSTRAINT FOREIGN KEY (secao) REFERENCES secao(idsec);
 ALTER TABLE empregado ADD CONSTRAINT FOREIGN KEY (chefe) REFERENCES empregado (idemp);
+
+
+-- Inserindo dados
+
+INSERT INTO secao VALUES 
+   (10, 'Fabrica', 'Recife'),
+   (20, 'Comercial', 'Recife'), 
+   (30, 'Marketing', 'Campina Grande'), 
+   (40, 'Planejamento', 'João Pessoa'), 
+   (50, 'Administração', 'Recife'), 
+   (60, 'Informática', 'Campina Grande'), 
+   (70, 'Recursos Humanos', 'João Pessoa');
+
+INSERT INTO empregado VALUES 
+   (3, 'Álvaro Dias', 50, 'Administrador', NULL, 2500, 0),
+   (1, 'Ana Paula', 10, 'Programador', 3, 3000, 10), 
+   (2, 'Gustavo Henrique', 70, 'Engenheiro', 1, 2500, 50), 
+   (4, 'Antônio Carlos', 10, 'Engenheiro', 3, 2400, 20),  
+   (5, 'Susana Silva', 20, 'Administrador', NULL, 2750, 30),
+   (6, 'Cláudio José', 60, 'Vendedor', 4, 1000, 50);
