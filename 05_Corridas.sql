e1be01dec0940763a56787dd11b93d28d80b262b
David Medeiros Santos <148370538+David-Mdrs@users.noreply.github.com>
2025-02-19 19:17:53 -0300

    Inserindo chaves estrangeiras

--- 05_Corridas.sql
+++ 05_Corridas.sql
@@ -50,3 +50,14 @@ CREATE TABLE IF NOT EXISTS piloto_pos (
     posicao INT,
     PRIMARY KEY (prova, piloto)
 );
+
+
+-- Definindo chaves estrangeiras
+
+ALTER TABLE piloto ADD CONSTRAINT FK_pe FOREIGN KEY (equipe) REFERENCES equipe (id);
+ALTER TABLE equipe ADD CONSTRAINT FK_ep FOREIGN KEY (pais) REFERENCES pais (id);
+ALTER TABLE circuito ADD CONSTRAINT FK_cp FOREIGN KEY (pais) REFERENCES pais (id);
+ALTER TABLE prova ADD CONSTRAINT FK_pc FOREIGN KEY (circuito) REFERENCES circuito (id);
+ALTER TABLE piloto_pos ADD CONSTRAINT FK_pp FOREIGN KEY (prova) REFERENCES prova (id);
+ALTER TABLE piloto_pos ADD CONSTRAINT FK_pp2 FOREIGN KEY (piloto) REFERENCES piloto (id);
+ALTER TABLE piloto ADD CONSTRAINT FK_pe2 FOREIGN KEY (pais) REFERENCES pais (id);
